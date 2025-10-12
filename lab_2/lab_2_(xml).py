599a22731eeebcbc3bac556984e531169c0fcb85
VolkovIS <volkovis2007@gmail.com>
2025-10-12 19:59:57 +0300

    4 commit

---
+++ lab_2/lab_2_(xml).py
@@ -0,0 +1,26 @@
+import xml.dom.minidom as minidom
+
+xml_file = open("currency.xml", 'r')
+file = xml_file.read()
+
+dom = minidom.parseString(file)
+dom.normalize()
+
+elements = dom.getElementsByTagName('Valute')
+namevalue = {}
+
+for node in elements:
+    for child in node.childNodes:
+        if child.nodeType == 1:
+            if child.tagName == "Name":
+                if child.firstChild.nodeType == 3:
+                    name = child.firstChild.data
+            if child.tagName == "Value":
+                if child.firstChild.nodeType == 3:
+                    value = child.firstChild.data
+    namevalue[name] = float(value.replace(',', '.'))
+
+
+print(namevalue)
+
+xml_file.close()
